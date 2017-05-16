8222d5ba82e6c727032d5ce9ca4d9f7289dfff45
Juan Carlos Perez Amin <juancarlosperezamin@yahoo.com>
2017-05-16 11:22:02 +0200

    Update server.js

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ app.use(function(err, req, res, next){
 });
 
 initDb(function(err){
-  console.log('Error connecting to MongoDB jc Message:\n'+err);
+  console.log('Error connecting to MongoDB Message:\n'+err);
 });
 
 app.listen(port, ip);
